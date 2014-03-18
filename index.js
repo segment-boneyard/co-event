6e8b12a9ef452ad01bfb92a52acee3b065397fce
TJ Holowaychuk <tj@vision-media.ca>
2014-03-18 14:37:27 -0700

    add Event "class" for v8

--- index.js
+++ index.js
@@ -20,7 +20,20 @@ module.exports = function(e){
     e.emit = function(type){
       if ('error' != type) emit.apply(e, arguments);
       var args = slice.call(arguments, 1);
-      done(null, { type: type, args: args });
+      done(null, new Event(type, args));
     };
   }
 };
+
+/**
+ * Initialize an event.
+ *
+ * @param {String} type
+ * @param {Arguments} args
+ * @api private
+ */
+
+function Event(type, args) {
+  this.type = type;
+  this.args = args;
+}
